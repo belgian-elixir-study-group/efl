c4bb180ee9582c31ace7e996a23de38a86bf6ddb
Yuri Leikind <yuri.leikind@gmail.com>
2016-01-29 09:10:01 +0100

    cleanup

--- completed_exercises/part1_sequential_elixir/optional/ex01.exs
+++
@@ -1,9 +0,0 @@
-
-sum =  fn a,b,c ->  a + b + c end
-# sum =  &( &1 + &2 + &3)
-
-IO.inspect sum.(1, 2, 3) #=> 6
-
-pair_tuple_to_list =  fn {a, b} ->  [a, b] end
-
-IO.inspect pair_tuple_to_list.( { 1234, 5678 } ) #=> [ 1234, 5678 ]
